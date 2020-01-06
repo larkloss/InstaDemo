1a1b10f7c647d1486cebc438818a0b7fa058f1e9
larkloss <dingzhao1995@gmail.com>
2020-01-06 21:46:40 +0800

    CRUD Finish
    CRUD 增删改查终于弄好了

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld
-
+from Insta.views import HelloWorld, PostsView, PostDeleteView, PostUpdateView, PostDetailView, PostCreateView
 urlpatterns = [
     path('', HelloWorld.as_view(), name = 'home'),
+    path('posts/', PostsView.as_view(), name = 'posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'make_post'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name = 'post_delete'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name = 'post_update'),
 ]
