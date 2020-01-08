4a05d4a28e70a4d247748a2935e9009b057d6a7e
larkloss <dingzhao1995@gmail.com>
2020-01-08 19:40:34 +0800

    Authentic功能
    注册与登录功能

--- Insta/views.py
+++ Insta/views.py
@@ -3,6 +3,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from Insta.models import Post
 
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 class HelloWorld(TemplateView):
     template_name = 'test.html'
@@ -15,10 +18,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -29,3 +33,8 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
